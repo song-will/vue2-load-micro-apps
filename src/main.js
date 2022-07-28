c66b7e42748d8c7bfafbd99530d8d91d6e7b28b5
song-will <735560190@qq.com>
2022-07-28 14:42:56 +0800

    stash

--- src/main.js
+++ src/main.js
@@ -1,8 +1,50 @@
+import '../public-path'
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import routes from './router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+let instance = null
+
+
+function renderApp({ container, data = {} } = {}) {
+  let router = new VueRouter({
+    base: window.__POWERED_BY_QIANKUN__ ? '/load-mirco-app' : '/',
+    mode: 'history',
+    routes,
+  });
+  console.log({ instance })
+  if (instance) return instance
+  return instance = new Vue({
+    router,
+    render: h => h(App, { props: data })
+  }).$mount(container ? container.querySelector('#app') : '#app')
+}
+console.log(window.__POWERED_BY_QIANKUN__)
+if (!window.__POWERED_BY_QIANKUN__) {
+  renderApp()
+}
+
+export async function mount(props) {
+  console.log('load micro app props', props)
+  renderApp(props);
+}
+
+export async function bootstrap(props) {
+  console.log('bootstrap----')
+}
+
+export async function unmount() {
+  instance.$destroy();
+  instance.$el.innerHTML = "";
+  instance = null;
+}
+
+// // 增加 update 钩子以便主应用手动更新微应用
+// export async function update(props) {
+//   renderPatch(props);
+// }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const { name } = require('./package');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 33904,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  configureWebpack: {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+    },
+  }
 })
